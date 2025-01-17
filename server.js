392a253734325fda7c285370848df4559d72a255
GuiPadoDevs <guilherme.padovine@wemov.com.br>
2025-01-17 11:20:19 -0300

    Updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
 app.use(cors({
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(authMiddleware);
 
 // Rotas
 app.use('/api/auth', require('./routes/authRoutes'));
